9f6c1817a07b4d2ac66530e541f025b962701e1c
scottchen98 <scott.chen1117@gmail.com>
2024-02-13 11:54:11 -0800

    move NavBar component to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import NavBar from "../components/navbar";
-
 export default function Home() {
-  return (
-    <div>
-      <NavBar />
-      This is the home page
-    </div>
-  );
+  return <div>This is the home page</div>;
 }
