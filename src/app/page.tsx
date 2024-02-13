7aeb4651371f121bb1609568b2709e854cdeefd2
scottchen98 <scott.chen1117@gmail.com>
2024-02-13 14:47:59 -0800

    create StoreSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./hero";
+import StoreSection from "./store-section";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <StoreSection />
     </main>
   );
 }
---
+++ src/app/store-section.tsx
@@ -0,0 +1,10 @@
+export default function StoreSection() {
+  return (
+    <div className="mt-8 h-[400px] bg-[#ebfff1] p-4">
+      <p className="text-xs tracking-widest">STORES & DINING</p>
+      <h2 className="text-3xl font-semibold tracking-wide">
+        Explore stores and restaurants at Crystal Mall.
+      </h2>
+    </div>
+  );
+}
