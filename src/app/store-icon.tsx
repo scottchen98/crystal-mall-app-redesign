1ab127aef555f2d2930d10ec019ec32d4c77f317
scottchen98 <scott.chen1117@gmail.com>
2024-02-14 10:50:59 -0800

    increase store icon and text size in larger viewports

--- src/app/store-icon.tsx
+++ src/app/store-icon.tsx
@@ -1,7 +1,9 @@
 export default function StoreIcon({ storeName }: { storeName: string }) {
   return (
-    <div className="relative mx-auto flex h-20 w-20 items-center justify-center rounded-full border-2 bg-white p-1">
-      <p className="text-center text-[10px] font-medium">{storeName}</p>
+    <div className="relative mx-auto flex h-20 w-20 items-center justify-center rounded-full border-2 bg-white p-1 lg:h-24 lg:w-24 2xl:h-32 2xl:w-32">
+      <p className="text-center text-[10px] font-medium 2xl:text-base">
+        {storeName}
+      </p>
     </div>
   );
 }
