7f804adf859e0678a3be3bf8c9f6829a45d1feb1
scottchen98 <scott.chen1117@gmail.com>
2024-02-13 19:49:50 -0800

    set max width

--- src/app/stores-section.tsx
+++ src/app/stores-section.tsx
@@ -24,29 +24,31 @@ const storeNames = [
 
 export default function StoresSection() {
   return (
-    <div className="mt-8 bg-secondary px-4 py-9 md:py-16">
-      <p className="mb-1 text-xs font-medium tracking-widest">
-        STORES & DINING
-      </p>
-      <div className="flex justify-between">
-        <h2 className="mb-10 text-3xl font-semibold tracking-wide md:text-4xl">
-          Embark on a shopping and dining adventure at Crystal Mall.
-        </h2>
-        <Link
-          href="/stores"
-          className="hidden font-semibold text-primary md:mr-8 md:block"
-        >
-          View all Stores & Dining
-        </Link>
+    <div className="bg-secondary">
+      <div className="mx-auto mt-8 max-w-7xl px-4 py-9 md:py-16">
+        <p className="mb-1 text-xs font-medium tracking-widest">
+          STORES & DINING
+        </p>
+        <div className="flex justify-between">
+          <h2 className="mb-10 text-3xl font-semibold tracking-wide md:text-4xl">
+            Embark on a shopping and dining adventure at Crystal Mall.
+          </h2>
+          <Link
+            href="/stores"
+            className="hidden font-semibold text-primary md:mr-8 md:block"
+          >
+            View all Stores & Dining
+          </Link>
+        </div>
+        <StoreIcons>
+          {storeNames.map((storeName) => (
+            <StoreIcon key={storeName} storeName={storeName} />
+          ))}
+        </StoreIcons>
+        <Button className="mt-10 w-full rounded-full py-6 text-base font-medium tracking-wider md:hidden">
+          View all stores & Dining
+        </Button>
       </div>
-      <StoreIcons>
-        {storeNames.map((storeName) => (
-          <StoreIcon key={storeName} storeName={storeName} />
-        ))}
-      </StoreIcons>
-      <Button className="mt-10 w-full rounded-full py-6 text-base font-medium tracking-wider md:hidden">
-        View all stores & Dining
-      </Button>
     </div>
   );
 }
