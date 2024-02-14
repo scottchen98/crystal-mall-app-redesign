8fc786d25af269e7acf631e9793891755c53978a
scottchen98 <scott.chen1117@gmail.com>
2024-02-14 10:42:13 -0800

    add background color to half of the container width on larger viewports

--- src/app/stores-section.tsx
+++ src/app/stores-section.tsx
@@ -24,31 +24,29 @@ const storeNames = [
 
 export default function StoresSection() {
   return (
-    <div className="bg-secondary">
-      <div className="mx-auto mt-8 max-w-7xl px-4 py-9 md:py-16">
-        <p className="mb-1 text-xs font-medium tracking-widest">
-          STORES & DINING
-        </p>
-        <div className="flex justify-between">
-          <h2 className="mb-10 text-3xl font-semibold tracking-wide md:text-4xl">
-            Embark on a shopping and dining adventure at Crystal Mall.
-          </h2>
-          <Link
-            href="/stores"
-            className="hidden font-semibold text-primary md:mr-8 md:block"
-          >
-            View all Stores & Dining
-          </Link>
-        </div>
-        <StoreIcons>
-          {storeNames.map((storeName) => (
-            <StoreIcon key={storeName} storeName={storeName} />
-          ))}
-        </StoreIcons>
-        <Button className="mt-10 w-full rounded-full py-6 text-base font-medium tracking-wider md:hidden">
-          View all stores & Dining
-        </Button>
+    <div className="mx-auto mt-8 max-w-7xl bg-[#f6fef6] px-4 py-9 md:relative md:bg-transparent md:py-16 md:before:absolute md:before:inset-0 md:before:-z-10 md:before:w-1/2 md:before:bg-[#f6fef6]">
+      <p className="mb-1 text-xs font-medium tracking-widest">
+        STORES & DINING
+      </p>
+      <div className="flex justify-between">
+        <h2 className="mb-10 text-3xl font-semibold tracking-wide md:text-4xl">
+          Embark on a shopping and dining adventure at Crystal Mall.
+        </h2>
+        <Link
+          href="/stores"
+          className="hidden font-semibold text-primary md:mr-8 md:block"
+        >
+          View all Stores & Dining
+        </Link>
       </div>
+      <StoreIcons>
+        {storeNames.map((storeName) => (
+          <StoreIcon key={storeName} storeName={storeName} />
+        ))}
+      </StoreIcons>
+      <Button className="mt-10 w-full rounded-full py-6 text-base font-medium tracking-wider md:hidden">
+        View all stores & Dining
+      </Button>
     </div>
   );
 }
