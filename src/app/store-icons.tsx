e10a97549198000272d8415cd0c5f0553073dc40
scottchen98 <scott.chen1117@gmail.com>
2024-02-13 17:42:49 -0800

    conditionally render 8 or 16 store icons depending on viewport

--- src/app/store-icons.tsx
+++ src/app/store-icons.tsx
@@ -1,11 +1,18 @@
+import React from "react";
+
 export default function StoreIcons({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="mx-auto flex flex-wrap items-center justify-center gap-8">
-      {children}
+    <div className="space-y-5">
+      <div className="grid grid-cols-4 gap-5 lg:grid-cols-8">
+        {React.Children.toArray(children).slice(0, 8)}
+      </div>
+      <div className="hidden lg:grid lg:grid-cols-8 lg:gap-5">
+        {React.Children.toArray(children).slice(8, 16)}
+      </div>
     </div>
   );
 }
