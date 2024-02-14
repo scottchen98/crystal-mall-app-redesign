8d590c6ac96a7f526f5d88ed9432de8623fd94c2
scottchen98 <scott.chen1117@gmail.com>
2024-02-13 18:23:13 -0800

    render the number of store icons based on viewport size

--- src/app/store-icons.tsx
+++ src/app/store-icons.tsx
@@ -7,10 +7,10 @@ export default function StoreIcons({
 }) {
   return (
     <div className="space-y-5">
-      <div className="grid grid-cols-4 gap-5 lg:grid-cols-8">
+      <div className="grid grid-cols-4 gap-5 md:grid-cols-8">
         {React.Children.toArray(children).slice(0, 8)}
       </div>
-      <div className="hidden lg:grid lg:grid-cols-8 lg:gap-5">
+      <div className="hidden md:grid md:grid-cols-8 md:gap-5">
         {React.Children.toArray(children).slice(8, 16)}
       </div>
     </div>
