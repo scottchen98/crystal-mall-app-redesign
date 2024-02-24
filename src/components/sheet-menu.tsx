25601b5c517657a497d1d13ad64f83bd8ae16631
scottchen98 <scott.chen1117@gmail.com>
2024-02-24 00:07:23 -0800

    add links to pages

--- src/components/sheet-menu.tsx
+++ src/components/sheet-menu.tsx
@@ -13,7 +13,19 @@ import {
 } from "@/components/ui/sheet";
 import { Separator } from "./ui/separator";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { href: "/stores", label: "Stores" },
+  { href: "/events", label: "Events" },
+  { href: "/visit", label: "Visit" },
+  { href: "/offers", label: "Offers" },
+  { href: "/about-us", label: "About Us" },
+];
+
 export function SheetMenu() {
+  const pathname = usePathname();
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -23,19 +35,24 @@ export function SheetMenu() {
       </SheetTrigger>
       <SheetContent>
         <SheetHeader className="mb-4">
-          <SheetTitle className="text-left">Crystal Mall</SheetTitle>
+          <SheetTitle className="mt-5 text-center">Crystal Mall</SheetTitle>
         </SheetHeader>
         <Separator />
-        <div className="my-7 flex flex-col gap-6">
-          <div>Stores</div>
-          <div>Events</div>
-          <div>Visit</div>
-          <div>Offers</div>
-          <div>About Us</div>
+        <div className="my-7 flex flex-col gap-3">
+          {links.map((link) => (
+            <Link key={link.label} href={link.href}>
+              <Button
+                variant={pathname === link.href ? "default" : "ghost"}
+                className="w-full"
+              >
+                {link.label}
+              </Button>
+            </Link>
+          ))}
         </div>
         <Separator />
         <SheetFooter className="mt-7">
-          <div className="mr-auto flex gap-3">
+          <div className="mx-auto flex gap-3">
             <Facebook size={24} />
             <Instagram size={24} />
           </div>
