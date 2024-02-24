12a5bfa7dd2efb00f14539859f9d3a992d115f72
scottchen98 <scott.chen1117@gmail.com>
2024-02-23 23:41:41 -0800

    create category-filter-dropdown component

---
+++ src/app/stores/category-filter-dropdown.tsx
@@ -0,0 +1,53 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+
+export default function CategoryFilterDropdown() {
+  return (
+    <Accordion
+      type="single"
+      collapsible
+      className="hidden w-full md:mt-5 md:block md:border-t"
+    >
+      <AccordionItem value="item-1" className="border-none">
+        <AccordionTrigger className="mb-2 font-semibold">
+          Category
+        </AccordionTrigger>
+        <AccordionContent>
+          <div className="flex flex-col gap-3">
+            <div>
+              <Checkbox id="terms" className="relative top-[2px] mr-2" />
+              <Label
+                htmlFor="terms"
+                className="text-sm font-normal leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Beauty & Hair Care
+              </Label>
+            </div>
+            <div>
+              <Checkbox id="terms" className="relative top-[2px] mr-2" />
+              <Label
+                htmlFor="terms"
+                className="text-sm font-normal leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Beauty & Hair Care
+              </Label>
+            </div>
+          </div>
+          <div className="mt-5 flex gap-4">
+            <Button variant="outline" className="h-11 w-full rounded-full">
+              Clear
+            </Button>
+            <Button className="h-11 w-full rounded-full">Apply</Button>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
